296a30f13249b6ef46b546ffd33effeb2667a465
sangho Kim <tkdgh97531@gmail.com>
2021-04-11 21:08:23 +0900

    Add header

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,14 @@
 import React, { useState } from "react"
+import Header from "@/components/form/campaigntypes/header"
 
 const App = (): React.FunctionComponentElement<Element> => {
   const [state] = useState(0)
-  return <h2>Hello with {state} </h2>
+  return (
+    <>
+      <Header />
+      <h2>Hello with {state} </h2>
+    </>
+  )
 }
 
 export default App
