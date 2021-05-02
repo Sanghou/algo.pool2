3976e72c6008c8de80f9703004aa1ea39440185f
sangho Kim <tkdgh97531@gmail.com>
2021-05-02 23:31:41 +0900

    Add base header

--- front/src/components/form/campaigntypes/header.tsx
+++ front/src/components/form/campaigntypes/header.tsx
@@ -2,7 +2,14 @@ import React from "react"
 import "./style.scss"
 
 const Header = (): React.FunctionComponentElement<Element> => {
-  return <header className="header"> Header </header>
+  return (
+    <header className="header">
+      <div className="header__wrapper">
+        <div className="logo"> logo component </div>
+        <div className="options"> Profile component </div>
+      </div>
+    </header>
+  )
 }
 
 export default Header
